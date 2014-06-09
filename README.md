479cd5d8b7a44b8a517eb1b941330567f417e67f
Sam Jordan <samuelfjordan@gmail.com>
2014-06-09 09:50:05 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+NetworkTest
+----------------------
+A short set of a couple of python scripts to test the basics of the currently connected network's quality.
+It displays and color codes according to quality:
+- packet loss
+- average ping
+- upload and download speeds
+
+**REQUIRES THE SPEEDTEST CLIENT SCRIPT:**
+you can get it (here)[https://github.com/sivel/speedtest-cli].
+
+Usage
+=======
+Run networktest as a shell script, `$ sh networktest`.
+This script includes a `-v` option if you wish to view the results as they come back.
+
+Alfred Workflow
+=======
+Also included is an Alfred workflow, which you can import to invoke the script more easily. To use it, you must have an alias set up: add the line `alias networktest='sh YOUR_PATH/networktest' to your .bash_profile or .bash_rc file.
---
+++ speedtestparser.py
@@ -0,0 +1,48 @@
+import sys
+import re
+from termcolor import colored
+if (len(sys.argv)==2):
+    fromfile = True
+else:
+    fromfile = False
+if not fromfile:
+    print colored("connecting with speedtest server...",attrs=["dark"])
+while 1:
+    try:
+        line = sys.stdin.readline().rstrip()
+    except KeyboardInterrupt:
+        break
+
+    if not line:
+        break
+
+    if(re.search("\[[0-9.]* km\]", line) != None):
+    	print colored(line,"cyan")
+    	if not fromfile:
+            print colored("testing download speed...",attrs=["dark"])
+    if(line.find("Cannot") != -1):
+    	print line
+    	sys.exit(-1)
+    if(line.find("Download: ") != -1):
+    	#print colored
+    	dl = line.split(" ")
+    	if(float(dl[1]) > 5.0):
+    		dlcolor = "green"
+    	elif(float(dl[1]) > 1.5):
+    		dlcolor = "yellow"
+    	else:
+    		dlcolor = "red"
+    	print colored(dl[0]+" "+dl[1]+" "+dl[2], dlcolor)
+    	if not fromfile:
+            print colored("testing upload speed...", attrs=["dark"])
+
+    if(line.find("Upload: ") != -1):
+    	#print colored
+    	ul = line.split(" ")
+    	if(float(ul[1]) > 2.0):
+    		ulcolor = "green"
+    	elif(float(ul[1]) > 1.0):
+    		ulcolor = "yellow"
+    	else:
+    		ulcolor = "red"
+    	print colored(ul[0]+" "+ul[1]+" "+ul[2], ulcolor)
